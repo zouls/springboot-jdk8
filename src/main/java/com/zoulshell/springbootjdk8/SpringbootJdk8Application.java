63bf5efecaff0f6929bf42ebb1b907128ab54c70
zoulshell <zoulshell@gmail>
2018-09-23 19:12:07 +0800

    init

---
+++ src/main/java/com/zoulshell/springbootjdk8/SpringbootJdk8Application.java
@@ -0,0 +1,12 @@
+package com.zoulshell.springbootjdk8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJdk8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJdk8Application.class, args);
+    }
+}
